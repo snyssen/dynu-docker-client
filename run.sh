a852b52f89da0aeb59def45ddc558f1e93a9a042
Simon Nyssen <simon.nyssen@mailfence.com>
2022-09-20 15:23:14 +0200

    Removes unused files

--- run.sh
+++
@@ -1,7 +0,0 @@
-source .env
-
-docker run -d --restart=always --name dynu-client \
-  -e DYN_HOSTNAME="${DYN_HOSTNAME}" \
-  -e DYN_USER="${DYN_USER}" \
-  -e DYN_PASS="${DYN_PASS}" \
-  deliganli/dynu:latest
